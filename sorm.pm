c6014652da67b3a2e22c52e7f7ff39cd93fc8109
Artem <Artem>
2017-10-27 13:16:38 +0300

    fixes

--- sorm.pm
+++ sorm.pm
@@ -3,11 +3,12 @@
 # mkdir /usr/abills/var/sorm/wi-fi/
 # mkdir /usr/abills/var/sorm/dictionaries/
 # echo "2017-01-01 00:00:00" > /usr/abills/var/sorm/last_admin_action
-# echo "2017-01-01 00:00:00" > /usr/abills/var/sorm/last_payments
+# echo "2017-08-01 00:00:00" > /usr/abills/var/sorm/last_payments
 #
 # $conf{BILLD_PLUGINS} = 'sorm';
-# $conf{ISP_ID} = '123'; # идентифакатор ИСП из "информация по операторам связи и их филалах"'
+# $conf{ISP_ID} = '1'; # идентифакатор ИСП из "информация по операторам связи и их филалах"'
 #
+# iconv -f UTF-8 -t CP1251 abonents.csv.utf > abonents.csv
 
 
 use strict;
@@ -24,32 +25,45 @@ our (
 );
 
 use Abills::Base qw/cmd _bp/;
+use Abills::Misc qw/translate_list/;
 use Users;
 use Dv;
 use Companies;
 use Finance;
+use Nas;
 
 my $User = Users->new($db, $Admin, \%conf);
 my $Company = Companies->new($db, $Admin, \%conf);
 my $Payments = Finance->payments($db, $Admin, \%conf);
-my $isp_id = $conf{ISP_ID};
+my $Nas = Nas->new($db, $Admin, \%conf);
+my $isp_id = $conf{ISP_ID} || 1;
+my $start_date = "01.08.2017 12:00:00";
 
-if ($argv->{DICTIONARIES} && $argv->{DICTIONARIES} eq 'NAS') {
+if ($argv->{DICTIONARIES}) {
+  # service_dictionary();
+  payments_type_dictionary();
+  docs_dictionary();
+  gates_dictionary();
+  ippool_dictionary();
+}
+elsif ($argv->{START}) {
+  my $users_list = $User->list({
+    COLS_NAME   => 1,
+    PAGE_ROWS   => 99999,
+  });
   
-}
-elsif ($argv->{DICTIONARIES} && $argv->{DICTIONARIES} eq 'serices') {
-
-}
-elsif ($argv->{DICTIONARIES} && $argv->{DICTIONARIES} eq 'serices') {
-
+  foreach (@$users_list) {
+    user_info_report($_->{uid});
+  }
 }
 else {
   check_admin_actions();
   check_system_actions();
   check_payments();
 }
-
+print "-------\n";
 send_changes();
+print "+++++++++\n";
 
 
 #**********************************************************
@@ -117,6 +131,10 @@ sub check_payments {
     UID         => '_SHOW',
 
     COLS_NAME   => 1,
+    PAGE_ROWS   => 99999,
+
+    SORT        => 'p.date', 
+    DESC        => 'DESC',
   });
   
   return 1 if ($Payments->{TOTAL} < 1);
@@ -146,18 +164,20 @@ sub user_info_report {
   $User->info($uid);
   $Dv->info($uid);
 
+  my ($family, $name, $surname) = split (' ', $User->{FIO});
+
   my @arr;
-  
-  $arr[0] = $isp_id;                                          # идентификатор филиала (справочник филиалов)
+
+  $arr[0] = $isp_id;                                    # идентификатор филиала (справочник филиалов)
   $arr[1] = $User->{LOGIN};                             # login
-  $arr[2] = ($Dv->{IP} ne '0.0.0.0') ? $Dv->{IP} : "";  # статический IP
+  $arr[2] = ($Dv->{IP} && $Dv->{IP} ne '0.0.0.0') ? $Dv->{IP} : "";  # статический IP
   $arr[3] = $User->{EMAIL};                             # e-mail
-  $arr[4] = $User->{PHONE};                             # телефон
-  $arr[5] = $Dv->{CID};                                 # MAC-адрес
-  $arr[6] = _date_format($User->{CONTRACT_DATE});       # дата договора
-  $arr[7] = $User->{CONTRACT_ID};                       # номер договора
+  $arr[4] = $User->{PHONE} || "";                       # телефон
+  $arr[5] = "";                                         # MAC-адрес
+  $arr[6] = _date_format($User->{REGISTRATION}) . ' 12:00:00';       # дата договора
+  $arr[7] = $User->{CONTRACT_ID} || $User->{LOGIN};     # номер договора
   $arr[8] = $User->{DISABLE};                           # статус абонента (0 - подключен, 1 - отключен)
-  $arr[9] = _date_format($User->{REGISTRATION});        # дата активации основной услуги
+  $arr[9] = _date_format($User->{REGISTRATION}) . ' 12:00:00';        # дата активации основной услуги
   $arr[10] = ($User->{EXPIRE} ne '0000-00-00' && $User->{EXPIRE} lt $DATE) ? _date_format($User->{EXPIRE}) : ""; # дата отключения основной услуги
 
 #физ лицо
@@ -165,25 +185,43 @@ sub user_info_report {
      
      $arr[11] = 0;             # тип абонента (0 - физ лицо, 1 - юр лицо)
 
-    my $passport = "";
-    if ($User->{PASPORT_NUM} && $User->{PASPORT_GRANT} && $User->{PASPORT_DATE}) {
-      $passport = $User->{PASPORT_NUM} . " " . $User->{PASPORT_GRANT} . " " . _date_format($User->{PASPORT_DATE});
+    my ($passport_num) = $User->{PASPORT_NUM} =~ m/(\d+)/;
+    my ($passport_ser) = $User->{PASPORT_NUM} =~ m/(\D+)/;
+    $passport_ser =~ s/\s//g if ($passport_ser);
+    $User->{PASPORT_GRANT} =~ s/\n//g;
+    $User->{PASPORT_GRANT} =~ s/\r//g;
+
+    if ($name && $surname && $family) { 
+      $arr[12] = '0';            # тип ФИО (0-структурировано, 1 - одной строкой) 
+      $arr[13] = $name;          # имя
+      $arr[14] = $surname;       # отчество
+      $arr[15] = $family;        # фамилия
+      $arr[16] = "";             # ФИО строкой
     }
     else {
-      print "No passport info for UID : $uid\n";
+      $arr[12] = '1';            # тип ФИО (0-структурировано, 1 - одной строкой) 
+      $arr[13] = "";             # имя
+      $arr[14] = "";             # отчество
+      $arr[15] = "";             # фамилия
+      $arr[16] = $User->{FIO};   # ФИО строкой
     }
 
-    $arr[12] = 1;              # тип ФИО (0-структурировано, 1 - одной строкой) 
-    $arr[13] = "";             # имя
-    $arr[14] = "";             # отчество
-    $arr[15] = "";             # фамилия
-    $arr[16] = $User->{FIO};   # ФИО строкой
     $arr[17] = "";             # дата рождения
-    $arr[18] = 1;              # тип паспортных данных (0-структурировано, 1-одной строкой)
-    $arr[19] = "";             # серия паспорта
-    $arr[20] = "";             # номер паспорта
-    $arr[21] = "";             # кем и когда выдан
-    $arr[22] = $passport;      # паспортные данные строкой
+
+    if ($passport_ser && $passport_num && $User->{PASPORT_GRANT}) {
+      $arr[18] = '0';            # тип паспортных данных (0-структурировано, 1-одной строкой)
+      $arr[19] = $passport_ser;  # серия паспорта
+      $arr[20] = $passport_num;  # номер паспорта
+      $arr[21] = $User->{PASPORT_GRANT} . " " . _date_format($User->{PASPORT_DATE});  # кем и когда выдан
+      $arr[22] = "";             # паспортные данные строкой
+    }
+    else {
+      $arr[18] = '1';            # тип паспортных данных (0-структурировано, 1-одной строкой)
+      $arr[19] = "";             # серия паспорта
+      $arr[20] = "";             # номер паспорта
+      $arr[21] = "";             # кем и когда выдан
+      $arr[22] = $User->{PASPORT_NUM} . " " . $User->{PASPORT_GRANT} . " " . $User->{PASPORT_DATE}; # паспортные данные строкой
+    }
     $arr[23] = 1;              # тип документа (спровочник видов документов)
     $arr[24] = "";             # банк абонента
     $arr[25] = "";             # номер счета абонента
@@ -242,7 +280,7 @@ sub user_info_report {
   $arr[42] = $address;        # адрес строкой
 
 #адрес устройства
-  $arr[43] = "";               # тип данных адреса устройства (0 - структурировано, 1 - одной строкой)
+  $arr[43] = 1;               # тип данных адреса устройства (0 - структурировано, 1 - одной строкой)
   $arr[44] = "";              # индекс
   $arr[45] = "";              # страна
   $arr[46] = "";              # область
@@ -252,7 +290,7 @@ sub user_info_report {
   $arr[50] = "";              # дом
   $arr[51] = "";              # корпус
   $arr[52] = "";              # квартира
-  $arr[53] = "";              # адрес строкой
+  $arr[53] = $address;        # адрес строкой
 
 
   my $string = "";
@@ -267,13 +305,92 @@ sub user_info_report {
 }
 
 #**********************************************************
-=head2 service_report($attr)
-
-convert 01.02.17 to 2017-02-01
+=head2 ippool_dictionary($attr)
 
 =cut
 #**********************************************************
-sub service_report {
+sub ippool_dictionary {
+
+  my $pools_list = $Nas->nas_ip_pools_list({
+    COLS_NAME   => 1,
+    PAGE_ROWS   => 99999,
+  });
+
+  foreach my $pool (@$pools_list) {
+    my $w=($pool->{ip}/16777216)%256;
+    my $x=($pool->{ip}/65536)%256;
+    my $y=($pool->{ip}/256)%256;
+    my $ip = "$w.$x.$y.0";
+    
+    my $mask = 32 - length(sprintf ("%b", $pool->{ip_count}));
+
+    my $string = '"' . $isp_id .'";'; 
+    $string .= '"' . $pool->{pool_name} . '";';
+    $string .= '"' . $ip . '";';
+    $string .= '"' . $mask . '";';
+    $string .= '"' . $start_date . '";';
+    $string .= '""' . "\n";
+
+    _add_report('pool', $string);
+  }
+print "IP pool dictionary formed.\n";
+  return 1;
+}
+
+#**********************************************************
+=head2 docs_dictionary($attr)
+
+=cut
+#**********************************************************
+sub docs_dictionary {
+  
+  my $string = '"' . $isp_id .'";"1";"01.08.2017";"";"паспорт"' . "\n";
+  _add_report('d_type', $string);
+
+  print "Docs dictionary formed.\n";
+  return 1;
+}
+
+#**********************************************************
+=head2 gates_dictionary($attr)
+
+=cut
+#**********************************************************
+sub gates_dictionary {
+  
+  my $string = '"' . $isp_id .'";"91.205.164.46";"01.08.2017";"";"Radius";"Россия";"Республика Крым";"Республика Крым";"г.Ялта";"ул. Соханя";"7";"7"' . "\n";
+  _add_report('gates', $string);
+
+  print "Gates dictionary formed.\n";
+  return 1;
+}
+
+#**********************************************************
+=head2 payments_type_dictionary($attr)
+
+=cut
+#**********************************************************
+sub payments_type_dictionary {
+  do ("/usr/abills/language/russian.pl");
+  my $types = translate_list($Payments->payment_type_list({ COLS_NAME => 1 }));
+
+  if ($conf{PAYSYS_PAYMENTS_METHODS}) {
+    foreach my $line (split (';', $conf{PAYSYS_PAYMENTS_METHODS})) {
+      my($id, $type) = split (':', $line);
+      push (@$types, {id => $id, name => $type} );
+    }
+  }
+
+  foreach (@$types) {
+    my $string = '"' . $isp_id .'";';
+    $string .= '"' . $_->{id} . '";';
+    $string .= '"' . $start_date . '";';
+    $string .= '"";';
+    $string .= '"' . $_->{name} . '"' . "\n";
+    _add_report('p_type', $string);
+  }
+
+  print "Payments types dictionary formed.\n";
   return 1;
 }
 
@@ -282,8 +399,10 @@ sub service_report {
 
 =cut
 #**********************************************************
-sub nas_info_report {
-  return 1;
+sub nas_dictionary {
+
+
+  _add_report('p_type', '"1";');
 }
 
 #**********************************************************
@@ -299,7 +418,7 @@ sub payment_report {
   
   my $string = '"' . $isp_id .'";';                             # идентификатор филиала из справочника
   $string   .= '"' . $attr->{method} . '";';                    # тип оплаты из сравочника
-  $string   .= '"' . ($attr->{contract_id} || "") . '";';       # номер договора
+  $string   .= '"' . ($attr->{login} || "") . '";';             # номер договора
   $string   .= '"' . $ip . '";';                                # статический IP
   $string   .= '"' . _date_format($attr->{datetime}) . '";';    # дата пополнения
   $string   .= '"' . $attr->{sum} . '";';                       # сумма пополнения
@@ -317,10 +436,14 @@ sub payment_report {
 #**********************************************************
 sub _add_report {
   my ($type, $string) = @_;
-
+print $string;
   my %reports = (
-    user    => "$var_dir/sorm/abonents/abonents.csv",
-    payment => "$var_dir/sorm/payments/payments.csv",
+    user    => "$var_dir/sorm/abonents/abonents.csv.utf",
+    payment => "$var_dir/sorm/payments/payments.csv.utf",
+    p_type  => "$var_dir/sorm/dictionaries/pay-types.csv.utf",
+    d_type  => "$var_dir/sorm/dictionaries/doc-types.csv.utf",
+    gates   => "$var_dir/sorm/dictionaries/gates.csv.utf",
+    pool    => "$var_dir/sorm/dictionaries/ip-numbering-plan.csv.utf",
   );
 
   my $filename = $reports{$type};
@@ -351,7 +474,7 @@ sub _date_format {
   # (substr($date, 0, 4), substr($date, 6, 2)) = (substr($date, 8, 2), substr($date, 0, 4));
   # $date =~ s/\-/\./g;
 
-  $date =~ s/(\d{4})-(\d{2})-(\d{2})\s([0-9:]+)/$3.$2.$1 $4/; 
+  $date =~ s/(\d{4})-(\d{2})-(\d{2})(.*)/$3.$2.$1$4/;
   return $date;
 }
 
@@ -361,5 +484,16 @@ sub _date_format {
 =cut
 #**********************************************************
 sub send_changes {
+  
+  # system('iconv -f UTF-8 -t CP1251 /usr/abills/var/sorm/abonents/abonents.csv.utf > /usr/abills/var/sorm/abonents/abonents.csv') if (-e "/usr/abills/var/sorm/abonents/abonents.csv.utf");
+  # system('iconv -f UTF-8 -t CP1251 /usr/abills/var/sorm/payments/payments.csv.utf > /usr/abills/var/sorm/payments/payments.csv') if (-e "/usr/abills/var/sorm/payments/payments.csv.utf");
+  # system('iconv -f UTF-8 -t CP1251 /usr/abills/var/sorm/dictionaries/pay-types.csv.utf > /usr/abills/var/sorm/dictionaries/pay-types.csv') if (-e "/usr/abills/var/sorm/dictionaries/pay-types.csv.utf");
+  # system('iconv -f UTF-8 -t CP1251 /usr/abills/var/sorm/dictionaries/doc-types.csv.utf > /usr/abills/var/sorm/dictionaries/doc-types.csv') if (-e "/usr/abills/var/sorm/dictionaries/doc-types.csv.utf");
+  # system('iconv -f UTF-8 -t CP1251 /usr/abills/var/sorm/dictionaries/gates.csv.utf > /usr/abills/var/sorm/dictionaries/gates.csv') if (-e "/usr/abills/var/sorm/dictionaries/gates.csv.utf");
+  # system('iconv -f UTF-8 -t CP1251 /usr/abills/var/sorm/dictionaries/ip-numbering-plan.csv.utf > /usr/abills/var/sorm/dictionaries/ip-numbering-plan.csv') if (-e "/usr/abills/var/sorm/dictionaries/ip-numbering-plan.csv.utf");
+
   return 1;
 }
+
+
+1
